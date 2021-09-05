57a50bb763e47d004514d0cfa08b72de046c4444
da-yooom <kdh941110@gmail.com>
2021-09-05 23:46:47 +0900

    🚧 Get Movie Query

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,1 +1,34 @@
-export default () => 'Detail';
+import { useParams } from 'react-router';
+import { gql, useQuery } from '@apollo/client';
+
+const GET_MOVIES = gql`
+  query getMovie($id: Int!) {
+    movie(id: $id) {
+      id
+      title
+      medium_cover_image
+      description_intro
+    }
+  }
+`;
+
+const Detail = () => {
+  const { id } = useParams();
+  const { loading, data } = useQuery(GET_MOVIES, {
+    variables: { id },
+  });
+
+  console.log(loading, data);
+
+  if (loading) {
+    return 'Loading...';
+  }
+
+  if (data && data.movie) {
+    return data.movie.title;
+  }
+
+  return 'Detail';
+};
+
+export default Detail;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,1 +1,31 @@
-export default () => <h1>Home</h1>;
+import { gql, useQuery } from '@apollo/client';
+import Movie from '../components/Movie';
+
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
+
+const Home = () => {
+  const { loading, data } = useQuery(GET_MOVIES);
+  console.log(loading, data);
+
+  return (
+    <>
+      <header>
+        <title>Apollo 2021</title>
+        <h2>My First GraphQL</h2>
+      </header>
+      {loading && <p>Loading...</p>}
+      {!loading &&
+        data.movies &&
+        data.movies.map((movie) => <Movie key={movie.id} {...movie} />)}
+    </>
+  );
+};
+
+export default Home;
