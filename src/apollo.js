963b9146454a317f90cc1749ba3f6051f5f57b53
da-yooom <kdh941110@gmail.com>
2021-09-07 15:00:12 +0900

    🗑 불필요한 주석 제거

--- src/apollo.js
+++ src/apollo.js
@@ -1,6 +1,5 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 
-// const cache = new InMemoryCache();
 const link = new createHttpLink({
   uri: 'http://localhost:4000/',
 });
